d241e47866e4fa26cc898493cf7576eba94cb325
syymm <shiyuming233@gmail.com>
2024-05-14 12:46:27 +0900

    Created register component

---
+++ src/Register.js
@@ -0,0 +1,12 @@
+import './Register.css'; 
+import RegisterComponent from './RegisterComponent';
+
+function Register() {
+    return (
+        <div className="registerpage">
+            <RegisterComponent />
+        </div>
+    );
+}
+
+export default Register;
